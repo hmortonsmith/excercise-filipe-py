3be438cd30b37dcc13f302414b885ef3e4e83778
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 14:09:13 +0100

    finished exercises

--- excercise_102/excercise_102.py
+++ excercise_102/excercise_102.py
@@ -1,19 +1,36 @@
 # Define a function full_name to take in first and last name
+def full_name(first_name, last_name):
+    name = f"{first_name} {last_name}"
+    return name.title()
 
 
 # Define a calculator_sum, to add 2 integers
+def calculator_sum(int1, int2):
+    sum_int = int(int1) + int(int2)
+    return sum_int
 
 
 # Define a calculator_subtract, to subtract 2 integers
+def calculator_subtract(int3, int4):
+    subtract_int = int(int3) - int(int4)
+    return subtract_int
 
 
 # Define a calculator_multiply, to multiply 2 integers
+def calculator_multiply(int5, int6):
+    multiply_int = int(int5) * int(int6)
+    return multiply_int
 
 
 # Define a area_square, to multiply 2 integers
+def area_square(length):
+    square_area = int(length) * int(length)
+    return square_area
 
 
 # Define a area_triangle, to multiply 2 integers
-
+def area_triangle(length, width):
+    triangle_area = int(length) * int(width) * 0.5
+    return triangle_area
 
 
