0abfd0d38d2466f703d6eff35589748ddeec5703
Hary Morton-Smith <HMorton-Smith@spartaglobal.com>
2019-05-29 13:26:09 +0100

    completed ex101

--- exercise_101.py
+++ exercise_101.py
@@ -4,12 +4,17 @@
 
 # Finish the  hello_human function
 def hello_human(name):
-    return "Hello Stacy, you are a humman"
+    return f"Hello {name}, you are a humman"
+
 
 # Finish the area of square function
 def area_square(length):
+    square_area = length * length
+    return square_area
     # todo write code to return the area of a square
-    pass
 
 
 # Define a function that take in 2 arguments to calculate the area of a triangle
+def area_triangle(height, width):
+    triangle_area = 0.5*height*width
+    return triangle_area
